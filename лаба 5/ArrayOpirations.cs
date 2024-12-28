af278b8c9ff074ae717aa7ff4af9e656c80a533d
iska6101 <iskazag90@gmail.com>
2024-12-28 11:03:25 +0400

    Add files via upload

---
+++ лаба 5/ArrayOpirations.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace лаба_5
+{
+    class ArrayOperations
+    {
+        private int[] array;
+        private int length;
+
+        // Конструктор без параметров (по умолчанию 10 элементов)
+        public ArrayOperations()
+        {
+            length = 10; // По умолчанию 10 элементов
+            array = CreateArray(length);
+        }
+
+        // Конструктор с параметрами
+        public ArrayOperations(int length)
+        {
+            try
+            {
+
+                if (length <= 0 || length > 10)
+                {
+                    throw new ArgumentOutOfRangeException("Длина массива должна быть от 1 до 10.");
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}\nБудет выводен массив по умолчанию");
+                this.length = 10;
+                array=CreateArray(this.length);
+            }
+            
+        }
+
+        // Метод для создания массива
+        private int[] CreateArray(int length)
+        {
+            int[] array = new int[length];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(10); // Генерируем случайные числа от 0 до 9
+            }
+            return array;
+        }
+
+        // Метод для печати массива
+        public void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+        public void PrintArray()
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+        // Метод для сортировки массива и измерения времени
+        public void SortAndTime()
+        {
+            Time(array);
+        }
+
+        private void Time(int[] array)
+        {
+            // Сортировка вставкой
+            int[] insertionArray = CopyArray(array);
+            var insertionWatch = System.Diagnostics.Stopwatch.StartNew();
+            InsertionSort(insertionArray);
+            insertionWatch.Stop();
+            Console.WriteLine("Отсортированный массив вставкой:");
+            Console.WriteLine($"Время сортировки вставкой: {insertionWatch.Elapsed.TotalMilliseconds} мс");
+            PrintArray(insertionArray);
+        }
+
+        private int[] CopyArray(int[] arr)
+        {
+            int[] copy = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                copy[i] = arr[i]; // Копируем каждый элемент по индексу
+            }
+            return copy;
+        }
+
+        private void InsertionSort(int[] insertionArray)
+        {
+            for (int i = 1; i < insertionArray.Length; i++)
+            {
+                int key = insertionArray[i];
+                int j = i - 1;
+
+                while (j >= 0 && insertionArray[j] > key)
+                {
+                    insertionArray[j + 1] = insertionArray[j];
+                    j--;
+                }
+                insertionArray[j + 1] = key;
+            }
+        }
+    }
+}
+
---
+++ лаба 5/GuesingGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using лаба_5.лаба_5;
+
+namespace лаба_5
+{
+    static class GuessingGame
+    {
+        public static void PlayGuessingGame()
+        {
+            double a = InputValidation.GetPositiveNumber("Введите число a (положительное): ");
+            double b = InputValidation.GetPositiveNumber("Введите число b (положительное): ");
+
+            if (Math.Cos(2 * a) >= 0)
+            {
+                double result = CalculateResult(a, b);
+                Console.WriteLine($"Вычисленный результат: {result:F2}");
+                Attempts(result);
+            }
+            else
+            {
+                Console.WriteLine("Условие для вычисления не выполнено. Попробуйте другие значения a и b.");
+            }
+        }
+
+        private static double CalculateResult(double a, double b)
+        {
+            return Math.Sin(Math.Log(b, 5)) / Math.Sqrt(Math.Cos(2 * a));
+        }
+
+        private static void Attempts(double correctAnswer)
+        {
+            int attempts = 3;
+
+            while (attempts > 0)
+            {
+                Console.Write("Введите ваш ответ: ");
+                double userAnswer;
+
+                while (!double.TryParse(Console.ReadLine(), out userAnswer))
+                {
+                    Console.WriteLine("Ошибка ввода. Пожалуйста, введите число.");
+                }
+
+                if (Math.Round(userAnswer, 2) == Math.Round(correctAnswer, 2))
+                {
+                    Console.WriteLine("Ответ верный! Вы выиграли!");
+                    return;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"Ответ неверный. У вас осталось {attempts} попыток.");
+                }
+            }
+        }
+    }
+}
+
